7a42b1e5f48aa60869a05535a6a4814d28a8d0c6
caviri <carlosvivarrios@gmail.com>
2024-03-09 10:22:13 +0100

    Allowing copying input files to output

--- startup.sh
+++ startup.sh
@@ -6,6 +6,12 @@ touch /odtp/odtp-logs/odtpS3UploadedDebugging.txt
 
 bash /odtp/odtp-component-client/odtp-app.sh 2>&1 | tee /odtp/odtp-logs/log.txt
 
+if [ "$TRANSFER_INPUT_TO_OUTPUT" == "TRUE" ]; then
+    echo "COPYING INPUT FILES TO OUTPUT"
+    cp -r /odtp/odtp-input/* /odtp/odtp-output
+fi
+
+
 echo "--- ODTP COMPONENT ENDING ---" >> /odtp/odtp-logs/log.txt
 
 # Leaving some time for logging to catch up
