9be02201f0e38500cfeeae45579ffa23484c8bb3
sabinem <5292683+sabinem@users.noreply.github.com>
2024-06-23 11:56:40 +0200

    set logging of commands flag in startup.sh

--- startup.sh
+++ startup.sh
@@ -6,8 +6,9 @@ touch /odtp/odtp-logs/odtpS3UploadedDebugging.txt
 
 bash /odtp/odtp-component-client/odtp-app.sh 2>&1 | tee /odtp/odtp-logs/log.txt
 
+set -x
 echo "--- ODTP COMPONENT ENDING ---" >> /odtp/odtp-logs/log.txt
-
+set +x
 # Leaving some time for logging to catch up
 sleep 10
 
