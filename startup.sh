9f1f1e81c65ac20683bbb44edb184d6878b32d81
sabinem <5292683+sabinem@users.noreply.github.com>
2024-06-23 16:59:46 +0200

    move flag in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 echo "RUNNING THE APP"
 touch /odtp/odtp-logs/log.txt
 touch /odtp/odtp-logs/odtpLoggerDebugging.txt
@@ -6,11 +7,11 @@ touch /odtp/odtp-logs/odtpS3UploadedDebugging.txt
 
 bash /odtp/odtp-component-client/odtp-app.sh 2>&1 | tee /odtp/odtp-logs/log.txt
 
-set -x
 echo "--- ODTP COMPONENT ENDING ---" >> /odtp/odtp-logs/log.txt
-set +x
+
 # Leaving some time for logging to catch up
 sleep 10
 
 # Zip logs
 zip -r /odtp/odtp-output/odtp-logs.zip odtp-logs
+set +x
