626ac5bff27d294a8bb5e19635381517b93b91ac
sabinem <5292683+sabinem@users.noreply.github.com>
2024-06-26 20:51:26 +0200

    feat(startup.sh) add proper logging to startup.sh

--- startup.sh
+++ startup.sh
@@ -4,13 +4,18 @@ set -e
 set -u
 set -o pipefail
 
-echo "RUNNING THE APP"
+source "/odtp/odtp-component-client/src/shell/log.sh"
+source "/odtp/odtp-component-client/src/shell/traceback.sh"
+
+odtp::print_info "setting up log files for the component"
 touch /odtp/odtp-logs/log.txt
 touch /odtp/odtp-logs/odtpLoggerDebugging.txt
 touch /odtp/odtp-logs/odtpS3UploadedDebugging.txt
 
+odtp::print_info "starting the odtp client app"
 bash /odtp/odtp-component-client/odtp-app.sh 2>&1 | tee /odtp/odtp-logs/log.txt
 
+# this comment is needed by the python logger to catch the end of the log
 echo "--- ODTP COMPONENT ENDING ---" >>/odtp/odtp-logs/log.txt
 
 # Leaving some time for logging to catch up
